3623c1598b68e4355af5076ea493d23cdf20b470
Mafabi Kennedy <63707180+kenoict@users.noreply.github.com>
2022-06-02 15:03:19 +0300

    Update

--- Online_Food_Odering_System/Foods_Ordering/settings.py
+++ Online_Food_Odering_System/Foods_Ordering/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v7)ouo@=dhswvnfs!@o$v07^m#+2l^1+c#n%bel2eqn!5f^fx^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://e-foodapp.herokuapp.com/]
 
 
 # Application definition
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'uganda/Kampala'
 
 USE_I18N = True
 
